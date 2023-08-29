e4d7dcb667d9bebcb756c8a5fb875ea6399adb2d
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2023-08-29 22:18:17 +0900

    initial commit

---
+++ lib/hakuboku.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hakuboku/version"
+
+module Hakuboku
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hakuboku/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Hakuboku
+  VERSION = "0.1.0"
+end
