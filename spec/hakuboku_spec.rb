2ff5fc6ecaa7079c43ee198fdc7b3ad82700048a
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2023-09-02 23:46:00 +0900

    Remove useless test code

--- spec/hakuboku_spec.rb
+++ spec/hakuboku_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Hakuboku do
   it "has a version number" do
     expect(Hakuboku::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
